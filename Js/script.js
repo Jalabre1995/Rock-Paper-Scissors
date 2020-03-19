7404ad803b3b16fc2663570932d90a7d70c9d6ef
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-03-18 21:18:38 -0400

    Updates

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,9 @@
 const disabledButtons = document.querySelectorAll("#btn");
 const startButton = docuemnt.querySelector("#startButton");
-const button =document.querySelector("#btn");
+const button = document.querySelector("#btn");
 const init = document.querySelectorAll("#btn");
+const playerSelection = playerAction;
+
 const Rock = "Rock";
 const Paper = "Paper";
 const Scissors = "Scissors";
@@ -10,18 +12,18 @@ let playerPoints = 0;
 let computerPoints = 0;
 let playerMovesCounter = 0;
 
-$(document).ready(function(){
-    $('#startButton').on('click', function(){
+$(document).ready(function () {
+    $('#startButton').on('click', function () {
         function styleStartButton() {
             this.classList.add('startClick');
-            disabledButtons.forEach(e => e.removeAttribute('disabled')); 
-            
+            disabledButtons.forEach(e => e.removeAttribute('disabled'));
+
         }
         startButton.addEventListener('click', styleStartButton);
         startButton.addEventListener('transitioned', removeStartClickClass);
 
         function removeStartClickClass(e) {
-            if(e.propertyName !=='transform') return;
+            if (e.propertyName !== 'transform') return;
             this.classList.remove('startClick');
         }
         ////Controls/////
@@ -30,33 +32,33 @@ $(document).ready(function(){
             playerMovesCounter++;
         }
 
-        button.forEach(e => e. addEventListener('click', controls));
-        
+        button.forEach(e => e.addEventListener('click', controls));
+
         ///Remove the controls/////
         function removeControls(e) {
-            if(e.propertyName !== 'transform') return;
+            if (e.propertyName !== 'transform') return;
             this.classList.remove('startClick');
         }
         button.forEach(e => e.addEventListener('transitioned', removeControls));
-        
+
         ////Getting the score////
         function getScore() {
             disabledButtons.forEach(e => e.setAttribute('disabled', true));
-            if(playerPoints > computerPoints) {
+            if (playerPoints > computerPoints) {
                 score = "Player won";
-            } else if (playerPoints<computerPoints) {
+            } else if (playerPoints < computerPoints) {
                 score = "Computer won";
-            }else if (playerPoints === computerPoints) {
+            } else if (playerPoints === computerPoints) {
                 score = "Tie!"
             }
-            h1. textContent = score;
+            h1.textContent = score;
             divStartButton.setAttribute('style', 'display: block;');
-             return score;
+            return score;
         }
         ////Computer's Decisions when Playing//////
-        function computerDescions() {
-            let pick = Math.floor(math.random() *3);
-            switch(pick) {
+        function  computerDecisions() {
+            let pick = Math.floor(math.random() * 3);
+            switch (pick) {
                 case 0:
                     return Rock;
                 case 1:
@@ -65,7 +67,40 @@ $(document).ready(function(){
                     return Scissors;
             }
         }
-        
+        function playerTurn() {
+            let playerAction = this.getAttribute('title');
+            function rounds() {
+                if (playerSelection === computerSelection) {
+                    return 0;
+                } else if (playerSelection === Rock && computerSelection === Scissors ||
+                    playerSelection === Paper && computerSelection === Rock ||
+                    playerSelection === Scissors && computerSelection === Paper) {
+                    return -1;
+                } else if (playerSelection === Rock && computerSelection === Scissors ||
+                    playerSelection === Paper && computerSelection === Rock ||
+                    playerSelection === Scissors && computerSelection === Paper) {
+                    return 1;
+                }
+            }
+            const playerSelection = playerAction;
+            const computerSelection = computerDecisions;
+            let subscore = playerTurn(playerSelection, computerSelection);
 
-    })
-})
+            if(subscore ===1) {
+                h1.textContent = 'You Win'
+                playerPoints++;
+            }else if (subscore === -1) {
+                h1.textContent = 'You Lose!'
+                computerPoints;
+            }else if(subscore === 0) {
+                h1.textContent = "Tie!";
+            }
+
+            playerSubscore.textContent = playerPoints;
+            computerSubscore.textContent = computerPoints;
+        }
+        if(playerMovesCounter === 5) {
+            getScore();
+        }
+    });
+});
